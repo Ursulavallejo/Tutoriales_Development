da864c38889693e631ad0b1397590d42874340ee
ursula_vallejo <ursulavallejo@gmail.com>
2022-01-10 22:30:42 +0100

    roter

--- reactCrashCourse/maximiliamTutorial/react-course-step2/src/App.js
+++ reactCrashCourse/maximiliamTutorial/react-course-step2/src/App.js
@@ -1,10 +1,27 @@
+import { Route, Routes } from "react-router-dom";
 
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
 
 function App() {
   return ( <div>
-        <h1>hello</h1>
+          <Routes>
+        <Route path='/'>
+            <AllMeetupsPage/>
+        </Route>
+          </Routes>
+      <Routes>
+          <Route path='/new-meetup'>
+              <NewMeetupPage/>
+          </Route>
+      </Routes>
+      <Routes>
+          <Route path='/favorites'>
+              <FavoritesPage/>
+          </Route>
+      </Routes>
       </div>
-
   );
 }
 
