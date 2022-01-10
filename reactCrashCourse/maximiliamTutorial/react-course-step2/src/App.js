a19a829879ef28c309ce0e0dc52d0f86378e7d32
ursula_vallejo <ursulavallejo@gmail.com>
2022-01-10 23:04:47 +0100

    Update App.js

--- reactCrashCourse/maximiliamTutorial/react-course-step2/src/App.js
+++ reactCrashCourse/maximiliamTutorial/react-course-step2/src/App.js
@@ -1,33 +1,27 @@
-import { BrowserRouter, Route} from "react-router-dom";
+import { Routes, Route} from "react-router-dom";
 
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
 
 function App() {
-  return (
-          <BrowserRouter>
+    return (
+        <Routes>
+            <Route path='/'>
+                <AllMeetupsPage/>
+            </Route>
+        </Routes>
+        <Routes>
+            <Route path='/new-meetup'>
+                <NewMeetupPage/>
+            </Route>
+        </Routes>
+        <Routes>
+            <Route path='/favorites'>
+                <FavoritesPage/>
+            </Route>
+        </Routes>
+        );
+        }
 
-        <Route path='/'>
-            <AllMeetupsPage/>
-        </Route>
-
-          </BrowserRouter>
-/*    <BrowserRouter>
-      <Routes>
-          <Route path='/new-meetup'>
-              <NewMeetupPage/>
-          </Route>
-      </Routes>
-    </BrowserRouter>
-    <BrowserRouter>
-      <Routes>
-          <Route path='/favorites'>
-              <FavoritesPage/>
-          </Route>
-      </Routes>
-    </BrowserRouter>*/
-  );
-}
-
-export default App;
+        export default App;
