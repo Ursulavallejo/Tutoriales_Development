6ec9b5e3f82e150be43989bf9f047eb3e8c7fb04
ursula_vallejo <ursulavallejo@gmail.com>
2022-01-11 09:02:56 +0100

    Update App.js

--- reactCrashCourse/maximiliamTutorial/react-course-step2/src/App.js
+++ reactCrashCourse/maximiliamTutorial/react-course-step2/src/App.js
@@ -1,21 +1,15 @@
-import {  Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
 
 function App() {
-    return (<div>
-            <Route path='/all-meetup'>
-                <AllMeetupsPage/>
-            </Route>
-            <Route path='/new-meetup'>
-                <NewMeetupPage/>
-            </Route>
-            <Route path='/favorites'>
-                <FavoritesPage/>
-            </Route>
-        </div>
+    return (<Routes>
+            <Route path='/' element={<AllMeetupsPage/>}/>
+            <Route path='/new-meetup' element={<NewMeetupPage/>}/>
+            <Route path='/favorites' element={<FavoritesPage/>}/>
+        </Routes>
         );
         }
 
